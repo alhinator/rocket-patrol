c4502bd7b78de3057f9b9f4f59d70b9b7c71b15c
alhinator <alex@leghart.org>
2024-01-18 18:03:11 -0800

    up to loading images

---
+++ src/scenes/Menu.js
@@ -0,0 +1,30 @@
+class Menu extends Phaser.Scene {
+    constructor(){
+        super("menuScene")
+    }
+
+    init(){
+
+    }
+
+    preload(){
+        // load images/tile sprites
+        this.load.image('rocket', './assets/rocket.png')
+        this.load.image('spaceship', './assets/spaceship.png')
+        this.load.image('starfield', './assets/starfield.png')
+    }
+
+    create(){
+        //this.add.text(20, 20, "Rocket Patrol Menu")
+        this.scene.start("playScene")
+
+
+    }
+
+    update(){
+
+
+    }
+
+
+}
